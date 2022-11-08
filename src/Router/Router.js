1a648392b038f3faa6e334d98ee854bc7c19f83a
Shoaib72 <shoaib.953872@gmail.com>
2022-11-08 16:40:01 +0000

    project-structure-and-footer-created

---
+++ src/Router/Router.js
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../Home/Home";
+import Main from "../Layout/Main";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/home',
+                element: <Home></Home>
+            }
+        ]
+    }
+])
+export default router;
