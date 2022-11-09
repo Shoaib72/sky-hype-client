5946ca6a96780cf0c1de5396a5618c03362cd127
Shoaib72 <shoaib.953872@gmail.com>
2022-11-09 00:20:42 +0000

    firebase-added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,13 +3,16 @@ import { Link } from 'react-router-dom';
 import img from './../../assets/login.ico'
 
 const Login = () => {
+    const handleLogIn = (event) => {
+        event.preventDefault()
+    }
     return (
         <div className="hero min-h-screen bg-slate-600 rounded-lg mb-5 relative">
             <h1 className="md:text-5xl font-bold text-white absolute md:top-10 top-10 text-2xl">Login now!</h1>
 
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-slate-500">
-                    <div className="card-body w-96 relative">
+                    <form onSubmit={handleLogIn} className="card-body w-96 relative">
                         <img className='w-10 h-10 absolute left-44 top-2' src={img} alt="" />
                         <div className="form-control">
                             <label className="label">
@@ -21,14 +24,14 @@ const Login = () => {
                             <label className="label">
                                 <span className="label-text text-white">Password</span>
                             </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <input type="password" placeholder="password" className="input input-bordered" />
 
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-outline btn-info"><span className='text-white'>Login</span></button>
+                            <button type='submit' className="btn btn-outline btn-info"><span className='text-white'>Login</span></button>
                         </div>
                         <p className='text-white mt-5'>Dont have an account? Please <Link className='text-gray-900 font-bold' to="/register">Register</Link></p>
-                    </div>
+                    </form>
                 </div>
 
 
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 const Register = () => {
     return (
         <div className="hero min-h-screen bg-slate-600 rounded-lg mb-5 relative">
-            <h1 className="md:text-5xl font-bold text-white absolute md:top-8 top-6 text-3xl">Login now!</h1>
+            <h1 className="md:text-5xl font-bold text-white absolute md:top-6 top-6 text-3xl">Register now!</h1>
 
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-slate-500">
-                    <div className="card-body w-96 relative">
+                    <form className="card-body w-96 relative">
 
                         <div className="form-control">
                             <label className="label">
@@ -30,10 +30,10 @@ const Register = () => {
 
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-outline btn-info"><span className='text-white'>Login</span></button>
+                            <button type='submit' className="btn btn-outline btn-info"><span className='text-white'>Register</span></button>
                         </div>
                         <p className='text-white mt-5'>Already have an account? Please <Link className='text-gray-900 font-bold' to="/login">Login</Link></p>
-                    </div>
+                    </form>
                 </div>
 
 
